1cab1d70417838732bbdb9a5caf21eaab8cd09c0
NxtWave <adarsh.dineshhegde@nxtwave.co.in>
2026-06-19 10:22:50 +0530

    Replace OAuth with service account auth

--- data/sheets_client.py
+++ data/sheets_client.py
@@ -6,17 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 import gspread
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-
-import os
-import json
-
-import gspread
-
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-
+from google.oauth2.service_account import Credentials
 
 SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets.readonly"
@@ -25,37 +15,13 @@ SCOPES = [
 
 def get_gspread_client():
 
-    credentials_json = json.loads(
-        os.getenv("CREDENTIALS")
+    creds = Credentials.from_service_account_file(
+    "service_account.json",
+    scopes=SCOPES
     )
 
-    token_file = "token.json"
-
-    creds = None
-
-    if os.path.exists(token_file):
-        creds = Credentials.from_authorized_user_file(
-            token_file,
-            SCOPES
-        )
-
-    if not creds or not creds.valid:
-
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-
-        else:
-            flow = InstalledAppFlow.from_client_config(
-                credentials_json,
-                SCOPES
-            )
-
-            creds = flow.run_local_server(port=0)
-
-        with open(token_file, "w") as token:
-            token.write(creds.to_json())
-
     return gspread.authorize(creds)
+
 
 def get_spreadsheet():
 
